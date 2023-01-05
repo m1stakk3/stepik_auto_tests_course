20a39a28360f7a9515ad7c2704ed80f522a658d7
Daniel Kirin <danielkirin@vk.com>
2023-01-05 14:28:25 +0300

    Signed-off-by: Daniel Kirin <danielkirin@vk.com>

---
+++ section_1/for_test.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+
+def find_element():
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+
+    try:
+        browser.get(link)
+        first_name = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[1]/input")
+        last_name = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[2]/input")
+        email = browser.find_element(By.XPATH, "/html/body/div/form/div[1]/div[3]/input")
+        req_fields = [first_name, last_name, email]
+        for field in req_fields:
+            field.send_keys("Sample")
+        button = browser.find_element(By.CSS_SELECTOR, ".btn")
+        button.click()
+        result = browser.find_element(By.TAG_NAME, "h1").text
+        assert "Congratulations! You have successfully registered!" == result
+
+    except NoSuchElementException:
+        print("Возникла ошибка NoSuchElementException")
+
+    finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
